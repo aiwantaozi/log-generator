999a4681e8d7165c40e4ab8295f195e758bab74e
Aiwantaozi <michelia.feng@gmail.com>
2019-07-08 16:16:48 +0800

    add format

--- main.go
+++ main.go
@@ -27,17 +27,31 @@ func main() {
 }
 
 func sayHelloToError(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(fmt.Sprintf(`{"log", "Hi %s, you got error log, time: %s"}`, randomdata.FullName(randomdata.RandomGender), time.Now().Format(time.RFC1123Z)))
+	fmt.Println(fmt.Sprintf(`{"log", "%s"}`, genHelloTo("error")))
 }
 
 func sayHelloToInfo(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(fmt.Sprintf(`{"log", "Hi %s, normanl log, time: %s"}`, randomdata.FullName(randomdata.RandomGender), time.Now().Format(time.RFC1123Z)))
+	fmt.Println(fmt.Sprintf(`{"log", "%s"}`, genHelloTo("info")))
+}
+
+func sayHelloToErrorText(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(genHelloTo("error"))
+}
+
+func sayHelloToInfoText(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(genHelloTo("info"))
+}
+
+func genHelloTo(level string) string {
+	return fmt.Sprintf("Hi %s, %s log, time: %s", randomdata.FullName(randomdata.RandomGender), level, time.Now().Format(time.RFC1123Z))
 }
 
 func server() {
 	fmt.Println("Starting Server, listen at port 9090")
-	http.HandleFunc("/error", sayHelloToError)
-	http.HandleFunc("/info", sayHelloToInfo)
+	http.HandleFunc("/error/json", sayHelloToError)
+	http.HandleFunc("/info/json", sayHelloToInfo)
+	http.HandleFunc("/error/text", sayHelloToErrorText)
+	http.HandleFunc("/info/text", sayHelloToInfoText)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
