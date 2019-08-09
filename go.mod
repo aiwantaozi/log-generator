1f9d31c88785eaa0049ccc7250db9057608d21c6
michelia feng <michelia.feng@gmail.com>
2019-08-09 13:25:27 +0800

    update vendor

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/aiwantaozi/log-generator
+
+require (
+	github.com/Pallinder/go-randomdata v1.2.0
+	github.com/sirupsen/logrus v0.0.0-20190807103436-de736cf91b92
+	github.com/urfave/cli v1.18.0
+	golang.org/x/sys v0.0.0-20190812073006-9eafafc0a87e
+)
--- main.go
+++ main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/Pallinder/go-randomdata"
+	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
 
-var VERSION = "v0.0.0-dev"
+const VERSION = "v0.0.0-dev"
+
+const jsonLogDir = "/var/mylogs/json"
+const textLogDir = "/var/mylogs/text"
+
+const infoJSONLogPath = "/var/mylogs/json/info"
+const errorJSONLogPath = "/var/mylogs/json/error"
+
+const infoTextLogPath = "/var/mylogs/text/info"
+const errorTextLogPath = "/var/mylogs/text/error"
+
+var infoJSONLog = log.New()
+var errorJSONLog = log.New()
+var infoTextLog = log.New()
+var errorTextLog = log.New()
+var jsonFormat = &log.JSONFormatter{DisableTimestamp: true}
 
 func main() {
 	app := cli.NewApp()
@@ -19,27 +34,69 @@ func main() {
 	app.Version = VERSION
 	app.Usage = "You need help!"
 	app.Action = func(c *cli.Context) error {
+		if err := os.MkdirAll(jsonLogDir, 0777); err != nil {
+			return err
+		}
+
+		fmt.Println("2")
+		if err := os.MkdirAll(textLogDir, 0777); err != nil {
+			return err
+		}
+
+		fmt.Println("3")
+		infoJSONLogFile, err := os.OpenFile(infoJSONLogPath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+		if err != nil {
+			return err
+		}
+		infoJSONLog.SetOutput(infoJSONLogFile)
+		infoJSONLog.SetFormatter(jsonFormat)
+		defer infoJSONLogFile.Close()
+
+		errorJSONLogFile, _ := os.OpenFile(errorJSONLogPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		errorJSONLog.SetOutput(errorJSONLogFile)
+		errorJSONLog.SetFormatter(jsonFormat)
+		defer errorJSONLogFile.Close()
+
+		infoTextLogFile, _ := os.OpenFile(infoTextLogPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		infoTextLog.SetOutput(infoTextLogFile)
+		defer infoTextLogFile.Close()
+
+		errorTextLogFile, _ := os.OpenFile(errorTextLogPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		errorTextLog.SetOutput(errorTextLogFile)
+		defer errorTextLogFile.Close()
+
 		server()
 		return nil
 	}
 
-	app.Run(os.Args)
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 }
 
 func sayHelloToError(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(fmt.Sprintf(`{"log", "%s"}`, genHelloTo("error")))
+	str := genHelloTo("error")
+	fmt.Println(fmt.Sprintf(`{"log", "%s"}`, str))
+	errorJSONLog.Errorln(str)
 }
 
 func sayHelloToInfo(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(fmt.Sprintf(`{"log", "%s"}`, genHelloTo("info")))
+	str := genHelloTo("info")
+	infoJSONLog.Infoln(str)
+	fmt.Println(fmt.Sprintf(`{"log", "%s"}`, str))
 }
 
 func sayHelloToErrorText(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(genHelloTo("error"))
+	str := genHelloTo("error")
+	fmt.Println(str)
+	errorTextLog.Errorln(str)
 }
 
 func sayHelloToInfoText(w http.ResponseWriter, r *http.Request) {
+	str := genHelloTo("infp")
 	fmt.Println(genHelloTo("info"))
+	infoTextLog.Errorln(str)
 }
 
 func genHelloTo(level string) string {
